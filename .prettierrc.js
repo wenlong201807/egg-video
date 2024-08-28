4216005b20647c5b3cc5b394fb90ae078af98b68
dragon <1573511441@qq.com>
2024-08-28 21:37:32 +0800

    ci: eslint & prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  jsxBracketSameLine: true,
+  bracketSameLine: true,
+};
